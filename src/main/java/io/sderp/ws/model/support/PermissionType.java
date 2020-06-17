68cfd0b64a983322de27172499b73691a1e87901
khh <khh@aetherit.io>
2020-06-17 18:57:13 +0900

    permissionType add

---
+++ src/main/java/io/sderp/ws/model/support/PermissionType.java
@@ -0,0 +1,5 @@
+package io.sderp.ws.model.support;
+
+public enum PermissionType {
+    ReportSubmit, ReportSearch, ReportTemplate, PlatformManagement, RoleManagement, UserManagement
+}
---
+++ web/src/type/PermissionType.js
@@ -0,0 +1,8 @@
+export const type = {
+    ReportSubmit: {code: 'ReportSubmit', text: '보고서 제출'},
+    ReportSearch: {code: 'ReportSearch', text: '보고서 검색'},
+    ReportTemplate: {code: 'ReportTemplate', text: '보고서 형식'},
+    PlatformManagement: {code: 'PlatformManagement', text: '플랫폼 관리'},
+    RoleManagement: {code: 'RoleManagement', text: '역할 관리'},
+    UserManagement: {code: 'UserManagement', text: '사용자 관리'}
+}
