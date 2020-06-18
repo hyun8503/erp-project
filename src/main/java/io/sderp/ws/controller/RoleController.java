bca76ca8d018a9f96a6568cbebbb09e483eb8ef3
khh <khh@aetherit.io>
2020-06-18 14:36:41 +0900

    role controller 수정

--- src/main/java/io/sderp/ws/controller/RoleController.java
+++ src/main/java/io/sderp/ws/controller/RoleController.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RestController
 @RequestMapping("/api/v1")
 public class RoleController {
     private RoleService roleService;
